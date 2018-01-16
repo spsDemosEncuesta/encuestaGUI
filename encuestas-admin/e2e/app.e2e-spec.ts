c8fe8a2bfc3644ef7113f7437cdbb954a4ee7627
Arturo Gonzalez <agonzalez@spsolutions.com.mx>
2018-01-16 13:19:44 -0600

    New Proyect
    1

--- encuestas-admin/e2e/app.e2e-spec.ts
+++ encuestas-admin/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('mean-encuestas-angular App', () => {
+describe('encuestas-admin App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- mean-encuestas-admin1/e2e/app.e2e-spec.ts
+++ mean-encuestas-admin1/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('mean-encuestas-angular App', () => {
+describe('mean-encuestas-admin App', () => {
   let page: AppPage;
 
   beforeEach(() => {
